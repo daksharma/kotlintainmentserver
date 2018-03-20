6b2a85013a36e800da9c5313d819c1e85e793197
Daksh Sharma <dashatrix@gmail.com>
2018-03-20 16:50:39 -0700

    yaml fix

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
 def hello():
-    return "Hello World! Testing New Python 3.6 Application"
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
 
-
-app.add_url_rule('/', view_func=hello)
 
 if __name__ == '__main__':
     app.run()
